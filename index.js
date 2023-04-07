82756653b889d9676266c65ad4abc6621d0fd70f
Isadora vargas <isadorasilveiradevragas6@gmail.com>
2023-04-06 22:00:22 -0300

    main

---
+++ index.js
@@ -0,0 +1,53 @@
+//exercio 1
+let a = 10 
+let b = 10
+
+console.log("variável b", b)
+
+b = 5
+console.log("variável a,b" + a,b)
+console.log("variável a,b" + a,b)
+console.log("variável a 'b'", a + "b")
+
+
+//exercício 2
+let x = 10
+let y = 20
+w = x
+y = w
+x = y
+
+console.log(x, y, w)
+
+
+//exercívio 3
+//let p = prompt("quantas horas você trabalha por dia?")
+//let t = prompt(" quanto você recebe por dia?")
+//alert(` você recebe ${t/p} por hora?`)
+
+
+//exercício de escreita de código
+let nome = null
+
+const idade = null
+const Gremio_campeão = true 
+
+alert (typeof Gremio_campeão)
+
+//nome = prompt('qual seu nome?')
+//idade = prompt('qual sua idade?')
+
+//minha parte 
+//console.log ("Olá" + nome + "você tem" + idade + "anos")
+
+let azul = prompt("Você esta usando roupa azul hoje?") 
+console.log("Você esta usando roupa azul hoje?" + azul)
+
+let gremio = prompt("você é do grêmio?")
+console.log("você é do gremio?" + gremio)
+
+let pizza = prompt("você gosta de pizza?")
+console.log("você gosta de pizza?" + pizza)
+
+
+
